6825de130e5a278a7a3dc0bcc60ca277eddedc1d
Anthony Nowell <anowell@gmail.com>
2017-06-01 21:39:15 -0600

    Uncomment examples

--- examples/classify.rs
+++ examples/classify.rs
@@ -6,20 +6,20 @@ use std::path::Path;
 
 fn main() {
     let file = Path::new("bw.jpg");
-    // let resp = file.classify().unwrap();
-    // println!("DEFAULT: {:#?}", resp);
+    let resp = file.classify().unwrap();
+    println!("DEFAULT: {:#?}", resp);
 
-    // let resp = file.classify_with(&DeepFace);
-    // println!("DEEPFACE: {:#?}", resp);
+    let resp = file.classify_with(&DeepFace);
+    println!("DEEPFACE: {:#?}", resp);
 
-    // let resp = file.classify_with(&Subreddit);
-    // println!("SUBREDDIT: {:#?}", resp);
+    let resp = file.classify_with(&Subreddit);
+    println!("SUBREDDIT: {:#?}", resp);
 
-    // let resp = file.classify_with(&Places365);
-    // println!("PLACES: {:#?}", resp);
+    let resp = file.classify_with(&Places365);
+    println!("PLACES: {:#?}", resp);
 
-    // let resp = file.classify_with(&RealEstate{ num_results: Some(3) });
-    // println!("REALESTATE: {:#?}", resp);
+    let resp = file.classify_with(&RealEstate{ num_results: Some(3) });
+    println!("REALESTATE: {:#?}", resp);
 
     let resp = file.classify_with(&Illustration::default());
     println!("ILLUSTRATION: {:#?}", resp);
